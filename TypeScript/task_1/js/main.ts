56d0b76cc0f7cf1976bb70ef081581f3ec17ea60
Genia888 <khalepo@gmail.com>
2025-09-10 21:21:40 +0200

    Initialize TypeScript project with Webpack configuration, TypeScript settings, and necessary dependencies

---
+++ TypeScript/task_1/js/main.ts
@@ -0,0 +1,22 @@
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [key: string]: any;
+}
+
+const teacher3: Teacher = {
+  firstName: 'John',
+  fullTimeEmployee: false,
+  lastName: 'Doe',
+  location: 'London',
+  contract: false,
+};
+
+console.log(teacher3);
+
+const app = document.createElement('pre');
+app.textContent = JSON.stringify(teacher3, null, 2);
+document.body.appendChild(app);
